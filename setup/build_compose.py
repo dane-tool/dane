769dbb08c4336c3f743d82da7839ad736f01b580
Parker Addison <pargrey@gmail.com>
2021-02-21 13:45:57 -0800

    Forgot to change `init` to `compose` in comment.

--- setup/build_compose.py
+++ setup/build_compose.py
@@ -101,7 +101,7 @@ def main(tool_dir, config_file, env_file, data_dir):
     built_file.parent.mkdir(parents=True, exist_ok=True)
     with open(built_file, 'w') as outfile:
         outfile.writelines([
-            '# Built by `build_compose.py` during `init` phase.\n',
+            '# Built by `build_compose.py` during `compose` phase of tool use.\n',
             '# Please do not edit, your changes will be overwritten during the next run.\n',
             '\n'
         ])
